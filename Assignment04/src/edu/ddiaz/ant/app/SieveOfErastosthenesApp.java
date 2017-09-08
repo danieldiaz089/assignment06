1adbeda29ed34f27f240409e5b48647896b1f89a
Daniel Diaz <danieldiaz089@brandeis.edu>
2017-09-07 21:20:42 -0400

    Push to new assignment06 repo
    Signed-off-by: Daniel Diaz <danieldiaz089@brandeis.edu>

---
+++ Assignment04/src/edu/ddiaz/ant/app/SieveOfErastosthenesApp.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package edu.ddiaz.ant.app;
+
+/**
+ * @author Daniel Diaz
+ * @version 1.0
+ */
+public class SieveOfErastosthenesApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Say hi
+		System.out.println("Hello there!");
+		System.out.println("Welcome to the Sieve of Erastosthenes App");
+
+		// show max arg
+		int max = Integer.parseInt(args[0]);
+		System.out.println("Max is " + max);
+
+		// print prime numbers
+		PrintPrimeNumbers(max);
+
+	}
+
+	private static void PrintPrimeNumbers(int max) {
+		// TODO Auto-generated method stub
+		int[] tmpArray = new int[max + 1];
+		int primeCtr = 0;
+
+		for (int i = 2; i <= Math.sqrt(max); i++) {
+			if (tmpArray[i] == 0) {
+				for (int j = i * i; j <= max; j += i) {
+					tmpArray[j] = 1;
+				}
+				
+				if (tmpArray[i] == 0 ) {
+					
+				}
+			}
+		}
+
+		int[] primeArray = tmpArray;
+		for (int i = 2; i < primeArray.length; i++) {
+			if (primeArray[i] == 0) {
+				System.out.print(i + ", ");
+				primeCtr++;
+			}
+		}
+		System.out.print("count = " + primeCtr);
+
+	}
+
+}
